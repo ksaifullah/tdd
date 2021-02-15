d89d0f9dcc9723c1fe2e6ceb1492368269fc5047
Travis Pereira <travis.pereira@kmart.com.au>
2021-02-15 11:33:22 +1100

    Add implementation for first test

--- src/find-intersection.js
+++ src/find-intersection.js
@@ -1,4 +1,14 @@
-const findIntersection = () => {};
+const findIntersection = (values) => {
+  const firstString = values[0];
+  const secondString = values[1];
+
+  const firstStringArray = firstString.split(',').map(x => x.trim());
+  const secondStringArray = secondString.split(',').map(x => x.trim());
+
+  const intersectionArray = firstStringArray.filter(x => secondStringArray.includes(x));
+  
+  return intersectionArray.join(',');
+};
 
 module.exports = {
   findIntersection,
